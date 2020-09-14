695708a6c4af1af98b0d28d1b65029f13418c4ae
InoveAlumnos <64550682+InoveAlumnos@users.noreply.github.com>
2020-09-14 10:11:40 -0300

    Add files via upload

--- resueltos/ejercicios_extra.py
+++ resueltos/ejercicios_extra.py
@@ -1,69 +1,67 @@
 #!/usr/bin/env python
 '''
 Listas [Python]
-Ejemplos de clase
+Ejercicios de clase
 ---------------------------
 Autor: Inove Coding School
-Version: 1.1
+Version: 1.2
 
 Descripcion:
-Programa creado para mostrar ejemplos prácticos de los visto durante la clase
+Programa creado para poner a prueba los conocimientos
+adquiridos durante la clase
 '''
 
 __author__ = "Inove Coding School"
 __email__ = "alumnos@inove.com.ar"
-__version__ = "1.1"
+__version__ = "1.2"
 
 import math
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-
 
 def practica_listas():
-    # 1) Crear una lista que contenga los nùmeros del -10 al 10
+    # 1) Crear una lista que contenga los nùmeros del -5 al 5
 
     # La lista solicitada es una secuencia numérica ordenada, se puede
     # crear utilizando range
 
     lista1 = []  # Lista vacia
 
-    # Crear una lista de rango -10 a 10 inclusive
-    for n in range(-10, 11):
+    # Crear una lista de rango -5 a 5 inclusive
+    for n in range(-5, 6):
         lista1.append(n)
 
     print('1:', lista1)
 
     # 2) Crear una lista que contengo unicamente los nùmeros
-    # pares entre -10 y 10
+    # impares entre -5 y 5
 
     lista2 = []  # Lista vacia
-    # Crear una lista de rango -10 a 10 inclusive de 
-    # solo nùmeros pares
-    for n in range(-10, 11):
-        if (n % 2) == 0:
+    # Crear una lista de rango -5 a 5 inclusive de 
+    # solo nùmeros impares
+    for n in range(-5, 6):
+        if (n % 2) != 0:
             lista2.append(n)
 
     print('2:', lista2)
 
-    # 3) De la lista1 filtrar los números positivos, es decir,
-    # crear una lista a partir de lista1 de solo números positivos
+    # 3) De la lista1 filtrar los números negativos, es decir,
+    # crear una lista a partir de lista1 de solo números negativos
 
     lista3 = []  # Lista vacia
-    # Filtrar numeros positivos
+    # Filtrar numeros negativos
     for n in lista1:
-        if n > 0:
+        if n < 0:
             lista3.append(n)
 
     print('3:', lista3)
 
-    # 4) De la lista1 filtrar los números mayores a 3, es decir,
-    # crear una lista a partir de lista1 de solo números mayores a 3
+    # 4) De la lista1 filtrar los números mayores a 2, es decir,
+    # crear una lista a partir de lista1 de solo números mayores a 2
 
     lista4 = []  # Lista vacia
-    # Filtrar numeros mayores a 3
+    # Filtrar numeros mayores a 2
     for n in lista1:
-        if n > 3:
+        if n > 2:
             lista4.append(n)
 
     print('4:', lista4)
@@ -101,31 +99,16 @@ def practica_listas():
     print('7:', lista6)
     print('7:', lista7)
 
-    # 8) De la lista1 calcular los números elevados al cuadrado
+    # 8) De la lista1 calcular multiplicar por dos todos los números
 
     lista8 = []  # Lista vacia
-    # Elevar al cuadrado
+    # Multiplicar por dos
     for n in lista1:
         lista8.append(n**2)
 
     print('8:', lista8)
 
-    # 9) Crear una lista "x" cuyo rango esté comprendido
-    # entre 0 y 4pi, que tenga 40 elementos
-    # Calcular la función seno de x
-    # x = np.linspace(0, 4*math.pi, 40)
-
-    # lista9 = []  # Lista vacia
-    # # Elevar al cuadrado
-    # for n in x:
-    #     lista9.append(math.sin(n))
-
-
-    # plt.plot(x, lista9, c='darkblue')
-    # plt.grid(ls='dashed')
-    # plt.show()
-
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    practica_listas() 
+    practica_listas()
